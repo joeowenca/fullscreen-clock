6d62d8fd76067579484e126e51d6dab9ed883cb8
Joe Owen <joe.a.owen@gmail.com>
2025-06-25 19:12:31 -0700

    Update text position and colour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,9 +15,11 @@ export default function Clock() {
 
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <h1 className="2xl:text-[180pt] xl:text-[140pt] lg:text-[100pt] md:text-[80pt] sm:text-[60pt] text-[40pt] text-shadow-[0_0px_30px_rgb(211_84_0_/_1)]">
-        {currentTime.toLocaleTimeString()}
-      </h1>
+      <div className="w-[80%]">
+        <h1 className="2xl:text-[180pt] xl:text-[130pt] lg:text-[90pt] md:text-[70pt] sm:text-[50pt] text-[30pt] text-shadow-[0_0px_30px_rgb(46_204_113_/_1)]">
+          {currentTime.toLocaleTimeString()}
+        </h1>
+      </div>
     </div>
   );
 }
