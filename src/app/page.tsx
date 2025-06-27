a89666bbd70f402b039219a230f6dae94d9d4ca9
Joe Owen <joe.a.owen@gmail.com>
2025-06-26 22:42:16 -0700

    Add windmill

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,40 @@ import { useState, useEffect } from "react";
 export default function Clock() {
   const [currentTime, setCurrentTime] = useState(new Date());
   const [temp, setTemp] = useState<number | null>(null);
+  const [hand, setHand] = useState(0);
 
-  useEffect(() => {
-    async function getWeather() {
+  const hands: string[] = ["\\", "|", "/", "-"];
+
+  async function fetchWeather() {
+    try {
       const res = await fetch("/api/weather");
       const data = await res.json();
       setTemp(data.temperature);
+    } catch (err) {
+      console.error("Failed to fetch weather:", err);
     }
+  }
 
+  useEffect(() => {
+    setCurrentTime(new Date());
+    fetchWeather();
+  }, []);
+
+  useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentTime(new Date());
-      getWeather();
+      fetchWeather();
     }, 1000);
 
     return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+    const intervalId2 = setInterval(() => {
+      setHand((prev) => prev + 1);
+    }, 200);
+
+    return () => clearInterval(intervalId2);
   }, []);
 
   const clockOptions: Intl.DateTimeFormatOptions = {
@@ -34,10 +54,13 @@ export default function Clock() {
 
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <div className="w-[80%] 2xl:text-[180pt] xl:text-[130pt] lg:text-[90pt] md:text-[70pt] sm:text-[50pt] text-[30pt] text-shadow-[0_0px_30px_rgb(46_204_113_/_1)]">
+      <div className="w-[80%] 2xl:text-[180pt] xl:text-[130pt] lg:text-[90pt] md:text-[70pt] sm:text-[50pt] text-[30pt] text-shadow-[0_0px_50px_rgb(46_204_113_/_1)]">
         <p>{currentTime.toLocaleTimeString("en-US", clockOptions)}</p>
         <p>{formattedDate}</p>
-        <p>{temp !== null ? ` ${temp}°C` : "Loading..."}</p>
+        <p className="float-left">
+          {temp !== null ? ` ${temp}°C` : "Loading..."}
+        </p>
+        <p className="float-right">{hands[hand % 4]}</p>
       </div>
     </div>
   );
