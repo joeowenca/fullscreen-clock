83ba7a01a63cd2711907d127f6a2dc4c78fc1df2
Joe Owen <joe.a.owen@gmail.com>
2025-06-26 22:12:54 -0700

    Add date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,13 +22,23 @@ export default function Clock() {
     return () => clearInterval(intervalId);
   }, []);
 
+  const clockOptions: Intl.DateTimeFormatOptions = {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  };
+
+  const month: string = String(currentTime.getMonth() + 1).padStart(2, "0");
+  const day: string = String(currentTime.getDate()).padStart(2, "0");
+
+  const formattedDate: string = `${month}/${day}`;
+
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <div className="w-[80%]">
-        <h1 className="2xl:text-[180pt] xl:text-[130pt] lg:text-[90pt] md:text-[70pt] sm:text-[50pt] text-[30pt] text-shadow-[0_0px_30px_rgb(46_204_113_/_1)]">
-          {currentTime.toLocaleTimeString()}
-          {temp !== null ? ` ${temp}°C` : "Loading..."}
-        </h1>
+      <div className="w-[80%] 2xl:text-[180pt] xl:text-[130pt] lg:text-[90pt] md:text-[70pt] sm:text-[50pt] text-[30pt] text-shadow-[0_0px_30px_rgb(46_204_113_/_1)]">
+        <p>{currentTime.toLocaleTimeString("en-US", clockOptions)}</p>
+        <p>{formattedDate}</p>
+        <p>{temp !== null ? ` ${temp}°C` : "Loading..."}</p>
       </div>
     </div>
   );
